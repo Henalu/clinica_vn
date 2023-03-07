aee81368b4a208bd09b2da35ec69d27d3dd1a5a6
Henalu <henaludebarros@hotmail.com>
2023-03-07 21:36:11 +0100

    basic nav styles

--- client/src/components/Header/Nav.js
+++ client/src/components/Header/Nav.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { AiOutlineMenu } from 'react-icons/ai'
 
 const Nav = () => {
   return (
     <nav>
-      Nav
+      <div className='nav-title-container'>
+        <h1 className='nav-title'>Clinica <span>viéitez núñez</span></h1>
+      </div>
+      <div>
+        <h2 className='nav-getintouch'>Pedir Cita</h2>
+        <span><AiOutlineMenu /></span>
+      </div>
     </nav>
   )
 }

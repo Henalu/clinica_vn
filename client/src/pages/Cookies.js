7549becaceea48a01030e7735b9375c15c5d80c1
Lydia Vega <lyvegama@gmail.com>
2023-03-14 14:35:13 +0100

    retoque de páginas del footer

--- client/src/pages/Cookies.js
+++ client/src/pages/Cookies.js
@@ -6,50 +6,53 @@ const Cookies = () => {
   return (
     <div className='page cookies'>
       <Nav />
-
-      <div>
-        <p>
-        Normativa sobre el uso de cookies
+<div
+  class="rounded-lg bg-neutral-100 p-6 text-neutral-700 shadow-lg dark:bg-neutral-600 dark:text-neutral-200 dark:shadow-black/30">
+  <h2 class="mb-5 text-3xl font-semibold">Política de cookies</h2>
+  <p>
+  Normativa sobre el uso de cookies
 Esta Política de Cookies es parte integrante del Aviso Legal y la Política de Privacidad de Gestión dentalDoctors, S.L. El acceso y la navegación en el sitio, o el uso de los servicios del mismo, implican la aceptación de las Condiciones Generales establecidas en nuestro Aviso Legal (y por tanto de la Política de Privacidad y Política de Cookies). Por favor, léelas atentamente.
 
 Te agradecemos que hayas decidido visitarnos. Queremos que tu experiencia en el sitio sea lo mejor posible, y por ese motivo hemos escrito esta Política de Cookies de la forma más transparente posible.
-
-
+</p> <hr
+    class="my-6 h-0.5 border-t-0 bg-neutral-200 opacity-100 dark:opacity-30" />
+  <p class="mb-4">
 ¿Qué es una Cookie?
-Las cookies son archivos que contienen pequeñas cantidades de información que se descargan en el dispositivo del usuario que se utiliza cuando visitas un sitio web. Su finalidad principal es reconocer al usuario cada vez que accede a clinicaprimitivoroig.com nos permite, además, mejorar la calidad y la usabilidad de nuestra web.
+Las cookies son archivos que contienen pequeñas cantidades de información que se descargan en el dispositivo del usuario que se utiliza cuando visitas un sitio web. Su finalidad principal es reconocer al usuario cada vez que accede a clinicavn.com nos permite, además, mejorar la calidad y la usabilidad de nuestra web.
 
-Las cookies son esenciales para el funcionamiento de Internet; no pueden dañar el equipo/dispositivo del usuario y, si se encuentran activadas en la configuración de tu navegador, nos ayudan a identificar y resolver posibles errores de funcionamiento de clinicaprimitivoroig.com.
-
- 
-
+Las cookies son esenciales para el funcionamiento de Internet; no pueden dañar el equipo/dispositivo del usuario y, si se encuentran activadas en la configuración de tu navegador, nos ayudan a identificar y resolver posibles errores de funcionamiento de clinicavn.com.
+</p> <hr
+    class="my-6 h-0.5 border-t-0 bg-neutral-200 opacity-100 dark:opacity-30" />
+  <p class="mb-4">
 Tipos de Cookies
 Hay diferentes tipos de cookies. Todos ellos trabajan de la misma manera, pero tienen pequeñas diferencias:
 
 Cookies de Sesión. Las cookies de sesión duran solamente por la duración de tu visita y se borran cuando cierras el Su finalidad principal es identificar el tipo de dispositivo, apoyar la seguridad sitio web o su funcionalidad básica. No contienen información personal que nos permita identificar a una persona.
 Cookies Persistentes o Permanentes: Se almacenan en el disco duro del dispositivo y nuestra web las lee cada vez que realizas una nueva visita a clinicadentalprimitivo.com; posee una fecha de caducidad o expiración determinada, cumplida la cual la cookie deja de funcionar. Nos permiten identificar tus acciones y preferencias; analizar las visitas y nos ayudan a comprender cómo llegan los usuarios a nuestra página y mejorar nuestros servicios.
-Cookies de Funcionalidad: Permiten a clinicaprimitivoroig.com recordar decisiones adoptadas por el usuario, como su login o identificador. La información de estas cookies recogen se anonimiza (es decir, no contiene ni tu nombre, dirección, u otros datos).
+Cookies de Funcionalidad: Permiten a clinicavn.com recordar decisiones adoptadas por el usuario, como su login o identificador. La información de estas cookies recogen se anonimiza (es decir, no contiene ni tu nombre, dirección, u otros datos).
 Cookies de Terceros. Las cookies de terceros son las cookies que instala un sitio web que no es el que estás visitando; por ejemplo, las usadas por redes sociales (como Facebook) o por complementos externos de contenido (como Google Maps). Además, algunas empresas de publicidad usan este tipo de archivos para realizar un seguimiento de tus visitas en cada sitio en el que se anuncian.
 Cookies Analíticas: Son cookies que tienen por finalidad el mantenimiento periódico y garantizar el mejor funcionamiento y servicio al usuario; recopilando datos de tu activilidad.
- 
-
-Uso de Cookies por parte de clinicaprimitivoroig.com
-Mediante el acceso a clinicaprimitivoroig.com aceptas de manera expresa que podamos usar este tipo de cookies en tus dispositivos. Si desactivas las cookies, puede que tu navegación por clinicaprimitivoroig.com no sea óptima y algunas de las utilidades que dispone clinicaprimitivoroig.com no funcionen correctamente.
-
- 
-
+</p> <hr
+    class="my-6 h-0.5 border-t-0 bg-neutral-200 opacity-100 dark:opacity-30" />
+  <p class="mb-4">
+Uso de Cookies por parte de clinicavn.com
+Mediante el acceso a clinicavn.com aceptas de manera expresa que podamos usar este tipo de cookies en tus dispositivos. Si desactivas las cookies, puede que tu navegación por clinicavn.com no sea óptima y algunas de las utilidades que dispone clinicavn.com no funcionen correctamente.
+</p> <hr
+    class="my-6 h-0.5 border-t-0 bg-neutral-200 opacity-100 dark:opacity-30" />
+  <p class="mb-4">
 a   Cookies propias
-
 Con el fin de reconocerte y prestarte un mejor servicio, nuestro sitio utiliza cookies (pequeños archivos de texto que tu navegador almacena) propias. Las ventajas que conlleva la aceptación de nuestras cookies se traduce en un ahorro de tiempo. Asimismo pueden ser utilizadas también para reconocerte entre visitas sucesivas y así adaptar el contenido que se te muestra, para obtener información acerca de la fecha y hora de tu última visita, medir algunos parámetros de tráfico dentro del propio sitio, y estimar el número de visitas realizadas, de manera que podamos enfocar y ajustar los servicios y promociones de forma más efectiva.
-
 Ninguna cookie permite que pueda contactarse con tu número de teléfono, tu dirección de correo electrónico o con cualquier otro medio de contacto. Ninguna cookie puede extraer información de tu disco duro o robar información personal.
-
+</p> <hr
+    class="my-6 h-0.5 border-t-0 bg-neutral-200 opacity-100 dark:opacity-30" />
+  <p class="mb-4">
 COOKIES ESTRICTAMENTE NECESARIAS
 Nombre	Propósito
 cookie_notice_acce pted	Cookies para almacenar su aceptación de nuestra política de cookies
- 
-
+</p> <hr
+    class="my-6 h-0.5 border-t-0 bg-neutral-200 opacity-100 dark:opacity-30" />
+  <p class="mb-4">
 b    Cookies de Terceros. Cookies de Google Analytics
-
 El sitio utiliza el servicio Google Analytics, que es prestado por Google, Inc., entidad cuya oficina principal está en 1600 Amphitheatre Parkway, Mountain View (California), CA 94043, Estados Unidos («Google»).
 
 Google Analytics utiliza cookies para ayudarnos a analizar el uso que hacen los usuarios del sitio. La información que genera la cookie acerca de tu uso del sitio (incluyendo tu dirección IP) será directamente transmitida y archivada por Google en sus servidores de Estados Unidos. Google usará esta información por cuenta nuestra con el propósito de seguir la pista de tu uso del sitio, recopilando informes de la actividad del sitio y prestando otros servicios relacionados con la actividad del sitio y el uso de Internet. Google podrá transmitir dicha información a terceros cuando así se lo requiera la legislación, o cuando dichos terceros procesen la información por cuenta de Google. Google no asociará tu dirección IP con ningún otro dato del que disponga Google.
@@ -57,23 +60,18 @@ Google Analytics utiliza cookies para ayudarnos a analizar el uso que hacen los 
 Si lo deseas puedes rechazar el tratamiento de los datos o la información rechazando el uso de cookies mediante la selección de la configuración apropiada de tu navegador. Sin embargo, si lo haces, puede ser que no puedas usar la plena funcionabilidad de este sitio. Al utilizar este sitio consientes el tratamiento tu información por Google en la forma y para los fines arriba indicados.
 
 Más  información  en   https://www.google.es/intl/es/policies/technologies/cookies/. Google ha desarrollado un complemento que puede instalar en su navegador si desea no ser rastreado por sus cookies: https://tools.google.com/dlpage/gaoptout
-
- 
-
+</p> <hr
+    class="my-6 h-0.5 border-t-0 bg-neutral-200 opacity-100 dark:opacity-30" />
+  <p class="mb-4">
 COOKIES ANALÍTICAS
 Nombre	Propósito
- 
- 
-
-_utma
+ _utma
 
 Se usa para distinguir usuarios y sesiones. La cookie se crea cuando se ejecuta la biblioteca JavaScript y no hay ninguna cookie     utma. La cookie se
- 
 
 actualiza cada vez que se envían datos a Google Analytics.
 
 _utmt	Se usa para limitar el porcentaje de solicitudes.
- 
  
 
 _utmb
@@ -83,9 +81,6 @@ Se usa para determinar nuevas sesiones o visitas. La cookie se crea cuando se ej
 
 se actualiza cada vez que se envían datos a Google Analytics.
 
- 
- 
-
 _utmc
 
 No se usa en ga.js. Se configura para interactuar con urchin.js. Anteriormente,   esta   cookie   actuaba   junto   con   la   cookie        utmb    para
@@ -93,20 +88,13 @@ No se usa en ga.js. Se configura para interactuar con urchin.js. Anteriormente, 
 
 determinar si el usuario estaba en una nueva sesión o visita.
 
- 
- 
 
 _utmz
 
 Almacena la fuente de tráfico o la campaña que explica cómo ha llegado el usuario al sitio. La cookie se crea cuando se ejecuta la biblioteca JavaScript y
  
-
 se actualiza cada vez que se envían datos a Google Analytics.
 
- 
- 
-
- 
 
 _utmv
 
@@ -115,9 +103,6 @@ Se usa para almacenar datos de variables personalizadas de visitante. Esta cooki
 
 _setVar  retirado.  La  cookie  se  actualiza  cada  vez  que  se  envían  datos  a Google Analytics.
 
- 
- 
-
 _ga
 
 Se usa para diferenciar entre usuarios y sesiones. La cookie se crea al cargar la librería javascript y no existe una versión previa de la cookie _ga. La cookie
@@ -125,18 +110,12 @@ Se usa para diferenciar entre usuarios y sesiones. La cookie se crea al cargar l
 
 se actualiza cada vez que envia los datos a Google Analytics.
 
- 
- 
-
 _gat
 
-Se usa para diferenciar entre los diferentes objetos de seguimiento creados en la sesión. La cookie se crea al cargar la librería javascript y no existe una versión previa de la cookie _gat. La cookie se actualiza cada vez que envia
- 
-
-los datos a Google Analytics.
-
- 
-
+Se usa para diferenciar entre los diferentes objetos de seguimiento creados en la sesión. La cookie se crea al cargar la librería javascript y no existe una versión previa de la cookie _gat. La cookie se actualiza cada vez que envia los datos a Google Analytics.
+</p> <hr
+    class="my-6 h-0.5 border-t-0 bg-neutral-200 opacity-100 dark:opacity-30" />
+  <p class="mb-4">
 Otras cookies de terceros
 Youtube
 
@@ -150,8 +129,9 @@ APISID GEUP
 HSID LOGIN_INFO PREFSSID VISITOR_INFO1_LIVE YSC
 _utma
 _utmz	Utilizadas por YouTube para almacenar información estadística de los usuarios que visualizan los vídeos y almacenar también sus preferencias (por ejemplo, el modo de visualización de los vídeos en youtube). Nuestro sitio utiliza de forma masiva la integración de vídeos con YouTube y por esa razón estás cookies están presentes.
- 
-
+</p> <hr
+    class="my-6 h-0.5 border-t-0 bg-neutral-200 opacity-100 dark:opacity-30" />
+  <p class="mb-4">
 Addtoany
 
 Addtoany es un servicio para que el usuario pueda compartir información de nuestra web a través de las redes sociales. Estas cookies forman parte de la aplicación AddtoAny y recopilan información sobre la actividad de los usuarios. Puede obtener más información en http://www.addtoany.com/privacy
@@ -164,7 +144,6 @@ _utmb	Se utiliza para saber el tiempo que pasa el usuario en nuestra página y 
 _utmc	Esta cookie se utiliza de forma conjunta con __utmb para saber si se establece una nueva sesión para el usuario.
 _utmz	Está cookie se utiliza para realizar la atribución de la visita, es decir desde dónde y cómo ha llegado el usuario a nuestra web.
 _cfduid	Cookie del plugin Addtoany, información sobre los puntos de conexión
- 
 
 Redes sociales
 
@@ -172,20 +151,17 @@ COOKIES Facebook
 Nombre	Propósito
 datr fr
 lu sb	La cookie DATR comprueba si la cuenta de usuario está siendo usada por un tercero para realizar fraudes. Cuando el usuario accede a una página que tiene un plug-in de Facebook instalado le manda a la plataforma el ID del cookie DATR, la URL de la web, la IP y la hora del acceso. Facebook no almacena esta información de forma detallada, sino agrupada y la eliminan después de 90 días.
- 
-
-Botón “Me gusta”
-
- 
+Botón “Me gusta” 
 
 COOKIES Instagram
 Nombre	Propósito
 mid
 rur s_network ig_ww ig_pr csrftoken	Cookies instaladas por la Red social Instagram
- 
-
+</p> <hr
+    class="my-6 h-0.5 border-t-0 bg-neutral-200 opacity-100 dark:opacity-30" />
+  <p class="mb-4">
 Configuración del usuario para evitar Cookies
-Desde clinicaprimitivoroig.com y en cumplimiento de la normativa legal vigente, ponemos a tu disposición la información que te permita configurar tu navegador/navegadores de Internet para mantener tu privacidad y seguridad en relación a las cookies. Por ello, te facilitamos la información y enlaces a los sitos de soporte oficiales de los principales navegadores para que puedas decidir si deseas o no aceptar el uso de cookies.
+Desde clinicavn.com y en cumplimiento de la normativa legal vigente, ponemos a tu disposición la información que te permita configurar tu navegador/navegadores de Internet para mantener tu privacidad y seguridad en relación a las cookies. Por ello, te facilitamos la información y enlaces a los sitos de soporte oficiales de los principales navegadores para que puedas decidir si deseas o no aceptar el uso de cookies.
 
 Así, puedes bloquear las cookies a través de las herramientas de configuración del navegador o bien puedes configurar tu navegador para que te avise cuando un servidor quiera guardar una cookie:
 
@@ -195,8 +171,16 @@ Si utilizas Chrome, en la sección de Opciones : Opciones avanzadas : Para saber
 Si utilizas Opera, en la opción de Seguridad y Privacidad, podrás configurar el Para saber más visita http://www.opera.com/help/tutorials/security/cookies/
 Si utilizas Safari, en la opción Preferencias : Seguridad o Privacidad, podrás configurar Para saber mas visita: hhttps://support.apple.com/kb/PH21411?viewlocale=es_ES&locale=es_ES
 Si desea deshabilitar Google Universal Analytics, puede instalar la siguiente extensión proporcionada por Google: https://tools.google.com/dlpage/gaoptout
-        </p>
-      </div>
+  </p>
+  <button
+    type="button"
+    data-te-ripple-init
+    data-te-ripple-color="light"
+    class="rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]">
+    Learn more
+  </button>
+</div>
+
       <Footer/> 
     </div>
   )
--- client/src/pages/PrivacyPolicy.js
+++ client/src/pages/PrivacyPolicy.js
@@ -6,53 +6,63 @@ const PrivacyPolicy = () => {
   return (
     <div className='page privacyPolicy'>
       <Nav />
-
-      <div>
+      <div
+        class="rounded-lg bg-neutral-100 p-6 text-neutral-700 shadow-lg dark:bg-neutral-600 dark:text-neutral-200 dark:shadow-black/30">
+        <h2 class="mb-5 text-3xl font-semibold">Política de privacidad</h2>
         <p>
-        En cumplimiento de lo dispuesto en el Reglamento (UE) 2016/679 del Parlamento Europeo y del Consejo de 27 de abril de 2016 relativo a la protección de las personas físicas en lo que respecta a tratamiento de datos personales y a la libre circulación de éstos datos, así como en el resto de normativa vigente en materia de protección de datos, el PROPIETARIO DE LA WEB le informa de lo siguiente:
-
-¿Quién es el responsable del tratamiento de sus datos?
-
-El responsable del tratamiento es el PROPIETARIO DE LA WEB cuyos datos identificativos se encuentran en el punto 1 del AVISO LEGAL.
-
- 
-
-¿Con que finalidad tratamos sus datos personales?
-
-El PROPIETARIO DE LA WEB tratará la información que facilite el usuario a través del Sitio Web con el fin de gestionar la prestación de los servicios solicitados y la gestión administrativa derivada de los mismos, así como remitir periódicamente newsletters, comunicaciones comerciales sobre servicios, eventos y noticias relacionadas con la actividad profesional del PROPIETARIO DE LA WEB cuando el usuario lo autorice, y para valorar la candidatura del usuario en el caso de que se recojan curriculums a través de la web.
-
- 
-
-¿Durante cuánto tiempo se conservarán sus datos?
-
-Los datos personales se conservarán mientras el usuario no solicite la supresión de los mismos y, en todo caso, durante los años necesarios para cumplir las obligaciones legales que establezca la normativa vigente, o para hacer frente a las posibles responsabilidades que pudiesen surgir.
-
- 
-
-¿Cuál es la legitimación para el tratamiento de sus datos?
-
-La legitimación del PROPIETARIO DE LA WEB para llevar a cabo el tratamiento de los datos de los usuarios se basa en el consentimiento del interesado. 
-
- 
-
-¿A qué destinatarios se comunicarán sus datos?
-
-Con carácter general los datos no serán cedidos a terceros, salvo que lo establezca la legislación vigente.
-
- 
-
-¿Cuáles son los derechos del usuario?
-
-El usuario tiene derecho a obtener confirmación sobre si el PROPIETARIO DE LA WEB está tratando datos personales que le conciernan o no. Tienen derecho a acceder a sus datos personales, así como a solicitar la rectificación de los datos inexactos o, en su caso, solicitar la supresión cuando, entre otros motivos, los datos ya no sean necesarios para los fines que motivaron su recogida. En determinadas circunstancias el usuario podrá solicitar la limitación del tratamiento de sus datos personales, en cuyo caso únicamente se conservaran bloqueados para el ejercicio o la defensa en reclamaciones. En determinadas circunstancias y por motivos relacionados con su situación particular, los usuarios podrán oponerse al tratamiento de sus datos, en cuyo caso el PROPIETARIO DE LA WEB dejará de tratar sus datos, salvo por motivos legítimos imperiosos, o el ejercicio o defensa de posibles reclamaciones. Además de los derechos anteriores, el usuario también tendrá el derecho a retirar el consentimiento y el derecho a presentar una reclamación ante la Autoridad de Control. Dispone de la información necesaria en www.agpd.es. Podrá ejercitar materialmente sus derechos aportando copia de su DNI o documento oficial que le identifique, mediante el envío de un mail o por correo postal dirigiéndose al PROPIETARIO DE LA WEB, cuyos datos identificativos se encuentran en el punto 1 del AVISO LEGAL.
-
- 
-
-¿Qué medidas de seguridad implantamos para proteger sus datos?
-
-El PROPIETARIO DE LA WEB tratará los datos del Usuario en todo momento de forma absolutamente confidencial y guardando el preceptivo deber de secreto respecto de los mismos, de conformidad con lo previsto en la normativa de aplicación, adoptando al efecto las medidas de índole técnica y organizativas necesarias que garanticen la seguridad de sus datos y eviten su alteración, pérdida, tratamiento o acceso no autorizado, habida cuenta del estado de la tecnología, la naturaleza de los datos almacenados y los riesgos a que están expuestos.
+          En cumplimiento de lo dispuesto en el Reglamento (UE) 2016/679 del Parlamento Europeo y del Consejo de 27 de abril de 2016 relativo a la protección de las personas físicas en lo que respecta a tratamiento de datos personales y a la libre circulación de éstos datos, así como en el resto de normativa vigente en materia de protección de datos, el PROPIETARIO DE LA WEB le informa de lo siguiente:
+          </p>
+          <hr
+          class="my-6 h-0.5 border-t-0 bg-neutral-200 opacity-100 dark:opacity-30" />
+        <p class="mb-4">
+          ¿Quién es el responsable del tratamiento de sus datos?
+          El responsable del tratamiento es el PROPIETARIO DE LA WEB cuyos datos identificativos se encuentran en el punto 1 del AVISO LEGAL.
+          </p>
+          <hr
+          class="my-6 h-0.5 border-t-0 bg-neutral-200 opacity-100 dark:opacity-30" />
+        <p class="mb-4">
+          ¿Con que finalidad tratamos sus datos personales?
+          El PROPIETARIO DE LA WEB tratará la información que facilite el usuario a través del Sitio Web con el fin de gestionar la prestación de los servicios solicitados y la gestión administrativa derivada de los mismos, así como remitir periódicamente newsletters, comunicaciones comerciales sobre servicios, eventos y noticias relacionadas con la actividad profesional del PROPIETARIO DE LA WEB cuando el usuario lo autorice, y para valorar la candidatura del usuario en el caso de que se recojan curriculums a través de la web.
+          </p>
+          <hr
+          class="my-6 h-0.5 border-t-0 bg-neutral-200 opacity-100 dark:opacity-30" />
+        <p class="mb-4">
+          ¿Durante cuánto tiempo se conservarán sus datos?
+          Los datos personales se conservarán mientras el usuario no solicite la supresión de los mismos y, en todo caso, durante los años necesarios para cumplir las obligaciones legales que establezca la normativa vigente, o para hacer frente a las posibles responsabilidades que pudiesen surgir.
+          </p>
+          <hr
+          class="my-6 h-0.5 border-t-0 bg-neutral-200 opacity-100 dark:opacity-30" />
+        <p class="mb-4">
+          ¿Cuál es la legitimación para el tratamiento de sus datos?
+          La legitimación del PROPIETARIO DE LA WEB para llevar a cabo el tratamiento de los datos de los usuarios se basa en el consentimiento del interesado.
+          </p>
+          <hr
+          class="my-6 h-0.5 border-t-0 bg-neutral-200 opacity-100 dark:opacity-30" />
+        <p class="mb-4">
+          ¿A qué destinatarios se comunicarán sus datos?
+          Con carácter general los datos no serán cedidos a terceros, salvo que lo establezca la legislación vigente.
+          </p>
+          <hr
+          class="my-6 h-0.5 border-t-0 bg-neutral-200 opacity-100 dark:opacity-30" />
+        <p class="mb-4">
+          ¿Cuáles son los derechos del usuario?
+          El usuario tiene derecho a obtener confirmación sobre si el PROPIETARIO DE LA WEB está tratando datos personales que le conciernan o no. Tienen derecho a acceder a sus datos personales, así como a solicitar la rectificación de los datos inexactos o, en su caso, solicitar la supresión cuando, entre otros motivos, los datos ya no sean necesarios para los fines que motivaron su recogida. En determinadas circunstancias el usuario podrá solicitar la limitación del tratamiento de sus datos personales, en cuyo caso únicamente se conservaran bloqueados para el ejercicio o la defensa en reclamaciones. En determinadas circunstancias y por motivos relacionados con su situación particular, los usuarios podrán oponerse al tratamiento de sus datos, en cuyo caso el PROPIETARIO DE LA WEB dejará de tratar sus datos, salvo por motivos legítimos imperiosos, o el ejercicio o defensa de posibles reclamaciones. Además de los derechos anteriores, el usuario también tendrá el derecho a retirar el consentimiento y el derecho a presentar una reclamación ante la Autoridad de Control. Dispone de la información necesaria en www.agpd.es. Podrá ejercitar materialmente sus derechos aportando copia de su DNI o documento oficial que le identifique, mediante el envío de un mail o por correo postal dirigiéndose al PROPIETARIO DE LA WEB, cuyos datos identificativos se encuentran en el punto 1 del AVISO LEGAL.
+          </p>
+          <hr
+          class="my-6 h-0.5 border-t-0 bg-neutral-200 opacity-100 dark:opacity-30" />
+        <p class="mb-4">
+          ¿Qué medidas de seguridad implantamos para proteger sus datos?
+          El PROPIETARIO DE LA WEB tratará los datos del Usuario en todo momento de forma absolutamente confidencial y guardando el preceptivo deber de secreto respecto de los mismos, de conformidad con lo previsto en la normativa de aplicación, adoptando al efecto las medidas de índole técnica y organizativas necesarias que garanticen la seguridad de sus datos y eviten su alteración, pérdida, tratamiento o acceso no autorizado, habida cuenta del estado de la tecnología, la naturaleza de los datos almacenados y los riesgos a que están expuestos.
         </p>
+        <button
+          type="button"
+          data-te-ripple-init
+          data-te-ripple-color="light"
+          class="rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]">
+          Leer más
+        </button>
       </div>
-      <Footer/> 
+      <Footer />
     </div>
   )
 }
